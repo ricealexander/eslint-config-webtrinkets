1e32b277c72e6d4a2fe5fad80725c1b5e5e34900
Alexander Rice <webdev@ricealexander.com>
2020-04-21 13:28:05 -0500

    :sparkles: Create curly example
    Create example which would throw errors with an incorrect curly implementation

---
+++ examples/curly.js
@@ -0,0 +1,10 @@
+const letters = new Set(
+  [...'abcdefghijklmnopqrstuvwxyz'],
+)
+
+if (letters.has('j')) {
+  console.info('J is for Jaguar')
+}
+
+if (letters.has('j')) console.info('J is for Jaguar')
+
