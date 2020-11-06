76bad91cc5c474005cb924be20a7a8757a090fe9
Alexander Rice <webdev@ricealexander.com>
2020-11-06 13:45:53 -0600

    :speech_balloon: Update ASI Hazards example
    Add example of array destructuring ASI hazard
    Update array method ASI hazard to use filter. Examples using forEach don't need to use spread because NodeLists have a forEach; Examples with map may be better to use Array.from which is optimal when spreading and mapping; filter doesn't have a better alternative. In the example I added, spreading and then filtering may be the ideal code to use.
    Trim down Top-Level Await example. I'd provided a full Puppeteer example, but having so much code distracted from the point.
    Replace the backtick example. Instead of a list of attributes, write a parser for a custom data format. It still has room for improvement because typically the data should be separate from code, but I can't think of a real example.
    Document reasoning for ESLint Rules

--- examples/asi-hazards.js
+++ examples/asi-hazards.js
@@ -1,36 +1,46 @@
-/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars               -- examples are partials */
+/* eslint-disable sonarjs/no-unused-collection -- examples are partials */
+/* eslint-disable import/no-unresolved         -- puppeteer example     */
+/* eslint-disable unicorn/string-content       -- array methods example */
 
-// Begin line with a parenthesis (
-// https://github.com/checkly/puppeteer-examples/blob/master/1.%20basics/get_text_value.js
+// start line with a parenthesis (top-level await)
 const puppeteer = require('puppeteer')
 
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://news.ycombinator.com/news')
-  const name = await page.$eval('.hnname > a', link => link.textContent)
-  console.log(name)
-  await browser.close()
+  // Do more stuff with puppetter…
 })()
 
 
-// begin line with a square bracket
-const activeItems = document.querySelectorAll('.active')
+// start line with square bracket (array methods)
+const links = document.querySelectorAll('a')
 
-;[...activeItems].forEach(item => {
-  item.classList.remove('.active')
-})
-
-
-// begin line with a backtick `
-const handleAttribute = console.log
-
-;`id
-className
-someOtherAttrs`
-  .split('\n')
-  .forEach(attribute => {
-    handleAttribute(attribute)
+;[...links]
+  .filter(link => link.href.startsWith('http://'))
+  .forEach(link => {
+    link.classList.add('.warn')
   })
 
 
+// start line with square bracket (array destructuring)
+const getCoordinates = () => { /* do stuff here */ }
+
+let x = 0
+let y = 0
+
+console.log(getCoordinates())
+;[x, y] = getCoordinates()
+
+
+// start line with a backtick (custom parsing)
+const parseLine = () => { /* do stuff here */ }
+const roles = []
+
+;`14: Director, 20040301 - 20040403
+129: Editor, 20171005 - 20171028
+169: Relations, 20191223 - 20200121`
+  .split('\n')
+  .forEach(line => {
+    roles.push(parseLine(line))
+  })
